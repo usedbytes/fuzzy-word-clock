4e7d661e443e44996d73e8502ed5f454a6f4429f
Brian Starkey <stark3y@gmail.com>
2016-02-21 17:56:32 +0000

    usb_cdc: Add some documentation to the header

--- usb_cdc.h
+++ usb_cdc.h
@@ -1,6 +1,23 @@
+/* USB CDC UART */
 
 int usb_init(void);
 
+/** Send the buffer over the USB serial port.
+ *
+ * Blocks until the transfer is complete
+ */
 void usb_usart_send(const char *buf, size_t len);
+
+/** Receive data from the USB serial port.
+ *
+ * Receive a maximum of 'len' bytes from the USB serial port into 'buf'
+ * Returns either after the given timeout (in milliseconds) or when the
+ * requested number of bytes have been received.
+ *
+ * A negative timeout means wait forever.
+ * Returns the number of bytes received.
+ */
 int usb_usart_recv(char *buf, size_t len, int timeout);
+
+/* Return "true" if the USB serial port is connected to a host */
 bool usb_usart_dtr(void);
