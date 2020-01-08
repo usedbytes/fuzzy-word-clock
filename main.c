6214ae65528d4f052bb8280d3f9a230fc6ad5f9b
Brian Starkey <stark3y@gmail.com>
2020-01-08 19:49:10 +0000

    Clean up and add TODOs/FIXMEs
    No warnings, remove some unused bits. Comments about improvements.

--- main.c
+++ main.c
@@ -167,6 +167,11 @@ uint16_t time_sub(uint16_t a, uint16_t b)
 void build_timebands()
 {
 	int i;
+	// FIXME: This can't handle a meal which overlaps midnight. The
+	// best fix is probably to make the timebands a linked list, and insert
+	// them in the correct place as we find them.
+	// That would also allow meals which are within (NEARLY + PAST) minutes
+	// of each other - which currently won't work either.
 	timebands[n_timebands] = (struct timeband){
 		.start = TIME(0, 0),
 		.sentence = 0,
@@ -251,7 +256,7 @@ void TIMER_16_0_Handler(void) {
 		bit += dir;
 
 #ifdef DEBUG
-		//LPC_GPIO->NOT[DBG_PORT] = DBG_PIN1;
+		LPC_GPIO->NOT[DBG_PORT] = DBG_PIN1;
 #endif
 		if (dir > 0 && bit == PWM_RESOLUTION) {
 			in_use_set = queued_set;
@@ -289,24 +294,6 @@ static void init_timer16(void)
 	timer->MCR = (1 << 9);
 	timer->TCR = 0x1;
 }
-
-const uint8_t gamma8[] = {
-	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-	1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-	2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-	5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-	10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-	17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-	25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-	37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-	51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-	69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-	90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-	115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-	144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-	177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-	215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
 
 void pwm_flip() {
 	int i;
@@ -383,6 +370,8 @@ void print_u32(uint32_t val)
 	usb_usart_print(buf);
 }
 
+// WAKEUP0 and TIMER_16_1 are used for the button.
+// It's pretty horrible, see button_init() for an overview.
 void WAKEUP0_Handler(void)
 {
 	uint32_t stat = LPC_GPIO_PIN_INT->IST;
@@ -457,6 +446,7 @@ void TIMER_16_1_Handler(void)
 	LPC_CT16B1->IR = status;
 }
 
+// TIMER_32_0 drives the animations, ticking every 10ms
 static void init_timer32()
 {
 	LPC_CTxxBx_Type *timer = LPC_CT32B0;
@@ -570,23 +560,8 @@ void button_init(void)
 	timer->PR = 65535;
 }
 
-void sleep_until(uint8_t day, uint16_t time)
-{
-	uint32_t sleep_start;
-	struct rtc_date date;
-	while (1) {
-		rtc_read_date(&date);
-		if (date.day == day) {
-			if (TIME(date.hours, date.minutes) >= time) {
-				return;
-			}
-		}
-
-		sleep_start = msTicks;
-		while ((ms_since(sleep_start)) < (1000 * 60));
-	}
-}
-
+// FIXME: All the command handling, parsing and printing should be better
+// encapsulated and moved elsewhere.
 int parse_time(char *buf, char **saveptr, uint16_t *time)
 {
 	char *tok = strtok_r(buf, ":", saveptr);
@@ -928,7 +903,6 @@ void handle_usart()
 
 int main(void)
 {
-	char buf[11] = "        \r\n";
 	SystemInit();
 	SystemCoreClockUpdate();
 	SysTick_Config(SystemCoreClock/1000);
@@ -985,6 +959,11 @@ int main(void)
 	int band = 0;
 	uint32_t demo_last_change = 0;
 	while(1) {
+		// FIXME: All of the state tracking and transition handling is
+		// pretty ugly here. Could come up with something better.
+		// Also, disabling the screen refresh and going into sleep when
+		// the screen is static would save a little bit of power
+		// (SysTick would need to be stopped too).
 		if (button == PRESSED) {
 			if (mode == NORMAL) {
 				mode = BLANKED;
